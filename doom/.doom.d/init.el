d73f570427730a9689447bc73df85a23e0997a49
Oemer Yildiz <oemer.yildiz@tuhh.de>
2020-09-23 18:44:41 +0200

    remove unused packages

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -94,9 +94,9 @@
        lookup              ; navigate your code and its documentation
        ;;lsp
        magit             ; a git porcelain for Emacs
-       make              ; run make tasks from Emacs
-       pass              ; password manager for nerds
-       pdf               ; pdf enhancements
+       ;; make              ; run make tasks from Emacs
+       ;; pass              ; password manager for nerds
+       ;; pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
@@ -168,7 +168,7 @@
        ;;yaml              ; JSON, but readable
 
        :email
-       (mu4e +gmail)
+       ;; (mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
