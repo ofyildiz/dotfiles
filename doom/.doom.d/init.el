12a6531a5f90a8cec377945c092e007c79bef607
Oemer Yildiz <oemer.yildiz@tuhh.de>
2020-10-08 08:56:28 +0200

    added mu4e and vterm

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -75,7 +75,7 @@
        eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -168,7 +168,7 @@
        ;;yaml              ; JSON, but readable
 
        :email
-       ;; (mu4e +gmail)
+       (mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
